133347dd2b80cba884a57d06b666e1cc7ee6ab39
Arunesh Saxena <aruneshsaxena@gmail.com>
2026-01-28 23:00:31 +0530

    moving all mfe to apps folder

--- apps/products/src/app/nx-welcome.tsx
+++ apps/products/src/app/nx-welcome.tsx
@@ -431,7 +431,7 @@ export function NxWelcome({ title }: { title: string }) {
         <div className="container">
           <div id="welcome">
             <h1>
-              <span> Hello there, kavi</span>
+              <span> Hello there, Kavi</span>
               Welcome {title} 👋
             </h1>
           </div>
